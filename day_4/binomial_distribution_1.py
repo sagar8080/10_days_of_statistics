4cad39ade41d781cd271e8ddf7b21dda30be805d
sagar8080 <sagar.uiet8@gmail.com>
2020-01-28 15:12:35 +0530

    Binomial Distribubtion

--- day_4/binomial_distribution_1.py
+++ day_4/binomial_distribution_1.py
@@ -1,2 +1,12 @@
-odds = list(map(float, raw_input().split()))
-print(odds[0], odds[1])
+def factorial(n):
+    return 1 if n == 0 else n*factorial(n-1)
+
+def combination(n, x):
+    return factorial(n) / (factorial(x) * factorial(n-x))
+
+def binomial(x, n, p):
+    return combination(n, x) * p**x * (1-p)**(n-x)
+
+l, r = list(map(float, input().split(" ")))
+odds = l / r
+print(round(sum([binomial(i, 6, odds / (1 + odds)) for i in range(3, 7)]), 3))
